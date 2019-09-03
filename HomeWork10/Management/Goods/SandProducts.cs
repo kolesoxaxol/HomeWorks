683fcf58e239e2587f104918eaaaa3cd4011ee46
VasylTravin <travin.vasilii@gmail.com>
2019-09-03 04:22:40 +0300

    Created goods class, started with SandProducts class

---
+++ HomeWork10/Management/Goods/SandProducts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management.Goods
+{
+    public class SandProducts : Goods
+    {
+        public SandProducts()
+        {
+
+        }
+
+        public SandProducts(string name, decimal price, int volume, DateTime dateOfRecieving, TimeSpan expirationTime) : base(name, price, volume, dateOfRecieving, expirationTime)
+        {
+            
+        }
+    }
+}
